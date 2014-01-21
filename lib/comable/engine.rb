9b78e4c7c46dddced62cebe9fb2dc8ac24a096d2
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-01-21 18:41:14 +0900

    顧客: カラムマッピング用の設定を追加

--- lib/comable/engine.rb
+++ lib/comable/engine.rb
@@ -3,5 +3,6 @@ module Comable
     isolate_namespace Comable
 
     config.product_default_column_names = %i( name code price caption )
+    config.customer_default_column_names = %i( family_name first_name )
   end
 end
