3c928f6015168a5744c011103edbfe48c28b79dc
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-03-08 01:44:41 +0900

    日本語に対応

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -22,7 +22,8 @@ module Dummy
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
+    I18n.enforce_available_locales = true
   end
 end
 
