4705e786700032284f966da43f2ac87710e17f78
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-03-09 08:16:28 +0900

    注文画面: テストを追加

---
+++ spec/dummy/spec/factories/orders.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :order, class: Comable::Order do
+    family_name "foo"
+    first_name "bar"
+    customer { FactoryGirl.build_stubbed(:customer) }
+  end
+end
---
+++ spec/views/orders/create.html.slim_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "comable/orders/create.html.slim" do
+  let (:order) { FactoryGirl.create(:order) }
+
+  before { assign(:order, order) }
+  before { render }
+
+  it "受注コードが表示されること" do
+    expect(rendered).to match(order.code)
+  end
+end
