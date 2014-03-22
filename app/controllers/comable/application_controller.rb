0b0033a6cc7c83378c1225e06da6c78a12e87dd3
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-03-22 20:22:23 +0900

    リファクタリング: ヘルパメソッドをコントローラから排除

--- app/controllers/comable/application_controller.rb
+++ app/controllers/comable/application_controller.rb
@@ -1,20 +1,5 @@
 module Comable
   class ApplicationController < ActionController::Base
-    def current_customer
-      @current_customer || load_customer
-    end
-
-    helper_method :current_customer
-
-    private
-
-    def load_customer
-      @current_customer = logged_in_customer
-      @current_customer ||= Comable::Customer.new(session)
-    end
-
-    def logged_in_customer
-      # Please override this method for logged in customer
-    end
+    include Comable::ApplicationHelper
   end
 end
--- app/helpers/comable/application_helper.rb
+++ app/helpers/comable/application_helper.rb
@@ -1,4 +1,18 @@
 module Comable
   module ApplicationHelper
+    def current_customer
+      @current_customer || load_customer
+    end
+
+    private
+
+    def load_customer
+      @current_customer = logged_in_customer
+      @current_customer ||= Comable::Customer.new(session)
+    end
+
+    def logged_in_customer
+      # Please override this method for logged in customer
+    end
   end
 end
