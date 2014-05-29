0316d3f0f249a523acb1446179b33acb7327686d
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-05-29 19:41:26 +0900

    Rails 4.0: テストが通るように修正

--- lib/comable/mapper.rb
+++ lib/comable/mapper.rb
@@ -1,6 +1,17 @@
 module Comable
   module Mapper
+    EXCEPTION_FOR_RAILS_4_0_METHODS = %i( all )
+    IGNORE_FOR_RAILS_4_0_METHODS = %i( build_default_scope )
+
     def method_missing(method_name, *args, &block)
+      if Rails::VERSION::MAJOR == 4 && Rails::VERSION::MINOR == 0
+        case method_name
+        when *EXCEPTION_FOR_RAILS_4_0_METHODS
+          return model.comable(comable_type)
+        when *IGNORE_FOR_RAILS_4_0_METHODS
+          return model.send(method_name, *args, &block)
+        end
+      end
       model.comable(comable_type).send(method_name, *args, &block)
     end
 
