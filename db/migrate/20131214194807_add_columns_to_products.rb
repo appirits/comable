d298ee6500ab9c8459fdf7e5b91aac4c665d41df
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-07-03 17:52:10 +0900

    SKU商品: カラムを追加

--- db/migrate/20131214194807_add_columns_to_products.rb
+++ db/migrate/20131214194807_add_columns_to_products.rb
@@ -4,6 +4,8 @@ class AddColumnsToProducts < ActiveRecord::Migration
     add_column_safety_to_products :code, :string, null: false
     add_column_safety_to_products :price, :integer
     add_column_safety_to_products :caption, :text
+    add_column_safety_to_products :sku_h_item_name, :string
+    add_column_safety_to_products :sku_v_item_name, :string
   end
 
   private
