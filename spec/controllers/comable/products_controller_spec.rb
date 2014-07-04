36805525121e9699d4d05060dcc2be2e8ce8a163
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-07-04 16:05:33 +0900

    SKU商品: テストを追加

--- spec/controllers/comable/products_controller_spec.rb
+++ spec/controllers/comable/products_controller_spec.rb
@@ -3,17 +3,37 @@ require 'spec_helper'
 describe Comable::ProductsController do
   render_views
 
-  let(:product) { FactoryGirl.create(:product) }
-
   before { request }
 
-  describe "GET 'index'" do
-    let(:request) { get :index }
-    its(:response) { should be_success }
+  context '通常商品' do
+    let(:product) { FactoryGirl.create(:product) }
+
+    describe "GET 'index'" do
+      let(:request) { get :index }
+      its(:response) { should be_success }
+    end
+
+    describe "GET 'show'" do
+      let(:request) { get :show, id: product.id }
+      its(:response) { should be_success }
+    end
   end
 
-  describe "GET 'show'" do
-    let(:request) { get :show, id: product.id }
-    its(:response) { should be_success }
+  context 'SKU商品' do
+    let(:product) { FactoryGirl.create(:product, :sku) }
+
+    describe "GET 'show'" do
+      let(:request) { get :show, id: product.id }
+      its(:response) { should be_success }
+    end
+  end
+
+  context 'SKU商品（横軸のみ）' do
+    let(:product) { FactoryGirl.create(:product, :sku_h) }
+
+    describe "GET 'show'" do
+      let(:request) { get :show, id: product.id }
+      its(:response) { should be_success }
+    end
   end
 end
