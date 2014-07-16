e97cd920c71bf9d0d5a03fafde1eed8b012c2a9e
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-07-16 12:09:57 +0900

    関連モデルについての記述をUtusemiから隔離

--- app/models/comable/order_detail.rb
+++ app/models/comable/order_detail.rb
@@ -1,6 +1,6 @@
 module Comable
   class OrderDetail < ActiveRecord::Base
-    belongs_to :stock, utusemi: :force
+    belongs_to :stock, utusemi: :force, class_name: Comable::Stock.model_name, foreign_key: Comable::Stock.foreign_key
     belongs_to :comable_order_deliveries, class_name: 'Comable::OrderDelivery'
 
     after_create :decrement_quantity!
