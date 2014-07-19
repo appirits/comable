e51ee1ed6906221d26cdee4dca19dca08c12f0cc
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-07-19 21:17:42 +0900

    リファクタリング: エイリアスクラスのファイルを移動

--- lib/comable.rb
+++ lib/comable.rb
@@ -1,7 +1,4 @@
 require 'comable/engine'
-require 'comable/product'
-require 'comable/customer'
-require 'comable/stock'
 require 'acts_as_comable_product/base'
 require 'acts_as_comable_customer/base'
 require 'acts_as_comable_stock/base'
