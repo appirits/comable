5e1e270e2de497698e62df61da4411e4d7e1b496
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-08-08 14:56:52 +0900

    商品詳細: カート投入数選択機能のテストを追加

--- spec/features/cart_spec.rb
+++ spec/features/cart_spec.rb
@@ -57,6 +57,31 @@ feature 'カート処理' do
     end
   end
 
+  shared_examples '商品を数量指定でカート投入できること' do
+    let(:quantity) { 7.to_s }
+
+    Steps 'ゲスト購入' do
+      Given '商品が存在するとき' do
+        visit comable.products_path
+        click_link subject.name
+        expect(page).to have_content subject.price
+      end
+
+      When '商品をカートに投入して' do
+        visit comable.product_path(subject)
+        choose subject.stocks.first.code if subject.sku?
+        select quantity, from: 'quantity'
+        click_button 'カートに入れる'
+        expect(page).to have_content I18n.t('comable.carts.add_product')
+      end
+
+      Then 'カートが更新されること' do
+        visit comable.cart_path
+        expect(page).to have_select('quantity', selected: quantity)
+      end
+    end
+  end
+
   shared_examples '商品の数量変更ができること' do
     let(:quantity) { 7.to_s }
 
@@ -92,6 +117,7 @@ feature 'カート処理' do
     given(:product) { FactoryGirl.create(:product, :with_stock) }
     subject { product }
     it_behaves_like '商品が購入できること'
+    it_behaves_like '商品を数量指定でカート投入できること'
     it_behaves_like '商品の数量変更ができること'
   end
 
@@ -99,6 +125,7 @@ feature 'カート処理' do
     given(:product) { FactoryGirl.create(:product, :sku) }
     subject { product }
     it_behaves_like '商品が購入できること'
+    it_behaves_like '商品を数量指定でカート投入できること'
     it_behaves_like '商品の数量変更ができること'
   end
 end
