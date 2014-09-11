b60b58d91791593d7bfee462f61f1ee4601a81e4
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-09-11 11:09:46 +0900

    backend: Implement products index

--- backend/app/controllers/comable/admin/products_controller.rb
+++ backend/app/controllers/comable/admin/products_controller.rb
@@ -3,6 +3,7 @@ require_dependency "comable/application_controller"
 module Comable
   class Admin::ProductsController < ApplicationController
     def index
+      @products = Comable::Product.all
     end
   end
 end
