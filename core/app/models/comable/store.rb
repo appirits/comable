903f350f4efbb3d4facb9295ce5920474909d2a5
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-09-26 22:18:56 +0900

    frontend: Display information of the store

--- core/app/models/comable/store.rb
+++ core/app/models/comable/store.rb
@@ -1,4 +1,13 @@
 module Comable
   class Store < ActiveRecord::Base
+    class << self
+      def instance
+        Comable::Store.first || Comable::Store.new(name: default_store_name)
+      end
+
+      def default_store_name
+        'Comable store'
+      end
+    end
   end
 end
