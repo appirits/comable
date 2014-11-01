50c2e6f5ef745e812717cf43d49d24b3e3435b36
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-11-02 00:34:27 +0900

    rubocop: Refactor methods at the carts controller

--- frontend/app/controllers/comable/carts_controller.rb
+++ frontend/app/controllers/comable/carts_controller.rb
@@ -2,14 +2,9 @@ module Comable
   class CartsController < Comable::ApplicationController
     rescue_from Comable::NoStock, with: :no_stock
 
-    def show
-    end
-
     def add
-      cart_item = Comable::Stock.where(id: params[:stock_id]).first
-      cart_item ||= Comable::Product.where(id: params[:product_id]).first
+      cart_item = find_cart_item
       return redirect_by_product_not_found unless cart_item
-      return redirect_by_product_not_found if cart_item.is_a?(Comable::Product) && cart_item.sku?
 
       current_customer.add_cart_item(cart_item, cart_item_options)
 
@@ -18,10 +13,10 @@ module Comable
     end
 
     def update
-      stock = Comable::Stock.where(id: params[:stock_id]).first
-      return redirect_by_product_not_found unless stock
+      cart_item = find_cart_item
+      return redirect_by_product_not_found unless cart_item
 
-      current_customer.reset_cart_item(stock, cart_item_options)
+      current_customer.reset_cart_item(cart_item, cart_item_options)
 
       flash[:notice] = I18n.t('comable.carts.update')
       redirect_to cart_path
@@ -37,6 +32,14 @@ module Comable
     def no_stock
       flash[:alert] = I18n.t('comable.errors.messages.products_soldout')
       redirect_to cart_path
+    end
+
+    def find_cart_item
+      cart_item = Comable::Stock.where(id: params[:stock_id]).first
+      cart_item ||= Comable::Product.where(id: params[:product_id]).first
+      return unless cart_item
+      return if cart_item.is_a?(Comable::Product) && cart_item.sku?
+      cart_item
     end
 
     def cart_item_options
