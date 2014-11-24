be2630eced6483036402bdaaa363a0424cbd41dd
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-11-24 18:46:27 +0900

    core: Add validations to address

--- core/app/models/comable/address.rb
+++ core/app/models/comable/address.rb
@@ -9,5 +9,14 @@ module Comable
       bill: 1,
       ship: 2
     }, scope: true
+
+    validates Comable::Customer.table_name.singularize.foreign_key, presence: true
+    validates :family_name, presence: true, length: { maximum: 255 }
+    validates :first_name, presence: true, length: { maximum: 255 }
+    validates :zip_code, presence: true, length: { maximum: 8 }
+    validates :state_name, presence: true, length: { maximum: 255 }
+    validates :city, presence: true, length: { maximum: 255 }
+    validates :detail, length: { maximum: 255 }
+    validates :phone_number, length: { maximum: 18 }
   end
 end
