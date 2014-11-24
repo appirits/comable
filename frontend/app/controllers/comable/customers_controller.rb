3eb0a799adc27c347f98b7c1f12f144a0ae0b273
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-11-24 18:48:53 +0900

    frontend: Add address page for customer

--- frontend/app/controllers/comable/customers_controller.rb
+++ frontend/app/controllers/comable/customers_controller.rb
@@ -4,5 +4,27 @@ module Comable
 
     def show
     end
+
+    def addresses
+      return unless request.put?
+
+      if current_customer.update_attributes(customer_params)
+        flash.now[:notice] = 'Success'
+      end
+    end
+
+    def customer_params
+      params.require(:customer).permit(
+        addresses_attributes: [
+          :family_name,
+          :first_name,
+          :zip_code,
+          :state_name,
+          :city,
+          :detail,
+          :phone_number
+        ]
+      )
+    end
   end
 end
