4a84f541df1632802af088a245d3873bed8d4d02
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-01-09 16:46:21 +0900

    Implement products search

--- core/app/models/comable/product.rb
+++ core/app/models/comable/product.rb
@@ -5,6 +5,21 @@ module Comable
     has_many :stocks, class_name: Comable::Stock.name
     after_create :create_stock
 
+    class << self
+      def search(keyword)
+        keyword.to_s.delete!('%')
+        return all if keyword.blank?
+
+        code = arel_table[:code].matches("%#{keyword}%")
+        name = arel_table[:name].matches("%#{keyword}%")
+        caption = arel_table[:caption].matches("%#{keyword}%")
+
+        relation = all
+        relation.where!(code.or(name.or(caption)))
+        relation
+      end
+    end
+
     def unsold?
       stocks.unsold.exists?
     end
