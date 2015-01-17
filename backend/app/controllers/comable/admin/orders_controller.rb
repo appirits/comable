caf78e407c14939c2ec1101cb53b628e3c3497de
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-01-18 03:08:08 +0900

    Implement pages of orders

---
+++ backend/app/controllers/comable/admin/orders_controller.rb
@@ -0,0 +1,19 @@
+require_dependency 'comable/admin/application_controller'
+
+module Comable
+  module Admin
+    class OrdersController < Comable::Admin::ApplicationController
+      before_filter :find_order, only: [:show, :edit, :update, :destroy]
+
+      def index
+        @orders = Comable::Order.complete.all.page(params[:page])
+      end
+
+      private
+
+      def find_order
+        @order = Comable::Order.find(params[:id])
+      end
+    end
+  end
+end
