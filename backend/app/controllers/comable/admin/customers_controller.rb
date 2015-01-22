4df325b5e0ea9101aff795e3c42ccc874fa0bfd6
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-01-23 00:37:12 +0900

    Implement edit page of a customer

--- backend/app/controllers/comable/admin/customers_controller.rb
+++ backend/app/controllers/comable/admin/customers_controller.rb
@@ -10,10 +10,35 @@ module Comable
         @customers = @customers.where!('email LIKE ?', "%#{params[:search_email]}%") if params[:search_email].present?
       end
 
+      def update
+        if @customer.update_attributes(customer_params)
+          redirect_to comable.admin_customer_path(@customer), notice: Comable.t('successful')
+        else
+          flash.now[:alert] = Comable.t('failure')
+          render :edit
+        end
+      end
+
       private
 
       def find_customer
         @customer = Comable::Customer.find(params[:id])
+      end
+
+      def customer_params
+        params.require(:customer).permit(
+          :email,
+          :password,
+          bill_address_attributes: [
+            :family_name,
+            :first_name,
+            :zip_code,
+            :state_name,
+            :city,
+            :detail,
+            :phone_number
+          ]
+        )
       end
     end
   end
