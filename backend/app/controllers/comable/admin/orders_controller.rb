089e3b926fc9c00e842ccad3bb195c88f81996a1
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-01-22 22:52:34 +0900

    Add the per page option to orders

--- backend/app/controllers/comable/admin/orders_controller.rb
+++ backend/app/controllers/comable/admin/orders_controller.rb
@@ -6,7 +6,7 @@ module Comable
       before_filter :find_order, only: [:show, :edit, :update, :destroy]
 
       def index
-        @orders = Comable::Order.complete.page(params[:page]).order('completed_at DESC')
+        @orders = Comable::Order.complete.page(params[:page]).per(15).order('completed_at DESC')
         @orders = @orders.where!('code LIKE ?', "%#{params[:search_code]}%") if params[:search_code].present?
       end
 
