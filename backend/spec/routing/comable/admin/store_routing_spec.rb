43cdb5fc2ffdadeffa14b842944507704a8a714c
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-01-25 00:20:04 +0900

    Fix tests to work for pages of a store

--- backend/spec/routing/comable/admin/store_routing_spec.rb
+++ backend/spec/routing/comable/admin/store_routing_spec.rb
@@ -2,20 +2,12 @@ describe Comable::Admin::StoreController do
   routes { Comable::Core::Engine.routes }
 
   describe 'routing' do
-    it 'routes to #new' do
-      expect(get: '/admin/store/new').to route_to('comable/admin/store#new')
-    end
-
     it 'routes to #show' do
       expect(get: '/admin/store').to route_to('comable/admin/store#show')
     end
 
     it 'routes to #edit' do
       expect(get: '/admin/store/edit').to route_to('comable/admin/store#edit')
-    end
-
-    it 'routes to #create' do
-      expect(post: '/admin/store').to route_to('comable/admin/store#create')
     end
 
     it 'routes to #update' do
