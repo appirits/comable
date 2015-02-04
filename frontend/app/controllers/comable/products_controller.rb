2748240d408e9d6baf5b6bda974c5270148699ca
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-02-04 18:43:05 +0900

    rubocop: Refactor to split the method

--- frontend/app/controllers/comable/products_controller.rb
+++ frontend/app/controllers/comable/products_controller.rb
@@ -1,6 +1,18 @@
 module Comable
   class ProductsController < Comable::ApplicationController
+    before_filter :load_category_and_products, only: :index
+
     def index
+      @products = @products.page(params[:page]).per(Comable::Config.products_per_page)
+    end
+
+    def show
+      @product = Comable::Product.find(params[:id])
+    end
+
+    private
+
+    def load_category_and_products
       @category = Comable::Category.where(id: params[:category_id]).first
       if @category
         subtree_of_category = Comable::Category.subtree_of(@category)
@@ -8,11 +20,6 @@ module Comable
       else
         @products = Comable::Product.search(params[:q])
       end
-      @products = @products.page(params[:page]).per(Comable::Config.products_per_page)
-    end
-
-    def show
-      @product = Comable::Product.find(params[:id])
     end
   end
 end
