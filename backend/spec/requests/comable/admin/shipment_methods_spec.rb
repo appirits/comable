e61d3275d618fa4c032a6112fd57ad9e8346383d
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-02-05 21:04:23 +0900

    Patch to the `comable` method.
    Because `*_path` that passed `script_name` option returns wrong value in Rails 4.2.
    refs: rails/rails@8cbcd19

--- backend/spec/requests/comable/admin/shipment_methods_spec.rb
+++ backend/spec/requests/comable/admin/shipment_methods_spec.rb
@@ -1,9 +1,13 @@
 describe 'ShipmentMethods' do
   sign_in_admin
 
+  # Patch to the `comable` method.
+  # Because `*_path` that passed `script_name` option returns wrong value in Rails 4.2.
+  let(:_comable) { comable.routes.url_helpers }
+
   describe 'GET /comable/admin/shipment_methods' do
     it 'works! (now write some real specs)' do
-      get comable.admin_shipment_methods_path
+      get _comable.admin_shipment_methods_path
       expect(response).to have_http_status(200)
     end
   end
