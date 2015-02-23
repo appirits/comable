6bd3e5de511e9134358355d3440df699daffc055
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-02-23 20:21:20 +0900

    Commonize the submit button

--- backend/app/helpers/comable/admin/application_helper.rb
+++ backend/app/helpers/comable/admin/application_helper.rb
@@ -1,6 +1,9 @@
 module Comable
   module Admin
     module ApplicationHelper
+      def link_to_save
+        link_to Comable.t('admin.actions.save'), 'javascript:$("form").submit()', class: 'btn btn-primary'
+      end
     end
   end
 end
