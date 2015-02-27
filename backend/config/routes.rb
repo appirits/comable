b1c26b4c48c9d25bbb8a9c9ddcf51936ae57de7a
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-02-28 01:49:20 +0900

    Implement the sign-in page for admin

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -16,6 +16,8 @@ Comable::Core::Engine.routes.draw do
     resources :payment_methods
     resource :store, controller: :store, only: [:show, :edit, :update]
 
-    devise_for :customer, path: :user, class_name: Comable::Customer.name, module: :devise
+    devise_for :customer, path: :user, class_name: Comable::Customer.name, module: :devise, controllers: {
+      sessions: 'comable/admin/user_sessions'
+    }
   end
 end
