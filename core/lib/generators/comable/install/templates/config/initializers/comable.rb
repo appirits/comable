20d573055419e516f30ca480651a5247ca6c1101
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-03-22 19:06:50 +0900

    Implement install generator

---
+++ core/lib/generators/comable/install/templates/config/initializers/comable.rb
@@ -0,0 +1,16 @@
+# Comable Configurations
+#
+Comable.setup do |config|
+  # Example:
+  #
+  # Uncomment to change devise strategies
+  # config.devise_strategies[:customer] = %i(
+  #   database_authenticatable
+  #   timeoutable
+  #   registerable
+  #   confirmable
+  #   recoverable
+  #   rememberable
+  #   trackable
+  # )
+end
