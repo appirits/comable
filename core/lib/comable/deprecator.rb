0c61265f93d7f3fb0bcd68887dd9fdf33f8a2b5b
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-03-25 20:44:45 +0900

    Rails 3.2: Fix tests to work

--- core/lib/comable/deprecator.rb
+++ core/lib/comable/deprecator.rb
@@ -1,7 +1,26 @@
-module Comable
-  class Deprecator < ActiveSupport::Deprecation
-    def initialize(deprecation_horizon = '0.4.0', gem_name = 'Comable')
-      super
+if Rails::VERSION::MAJOR > 3
+  module Comable
+    class Deprecator < ActiveSupport::Deprecation
+      def initialize(deprecation_horizon = '0.4.0', gem_name = 'Comable')
+        super
+      end
     end
+  end
+else
+  # TODO: Deprecated itself!
+  module Comable
+    class Deprecator
+      include Singleton
+    end
+  end
+
+  class Module
+    def deprecate_with_deprecator(*method_names)
+      options = method_names.extract_options!
+      options.delete(:deprecator) if options.present?
+      deprecate_without_deprecator
+    end
+
+    alias_method_chain :deprecate, :deprecator
   end
 end
