ff58ad369e3ea06729ca89a9bd551c6a25200a82
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-04-17 10:36:00 +0900

    Improve the column name of associations to import

--- core/app/models/comable/stock/csvable.rb
+++ core/app/models/comable/stock/csvable.rb
@@ -7,13 +7,19 @@ module Comable
 
       included do
         comma do
-          __association__ product: :code
-          __association__ product: :name
+          product_code
           code
           quantity
           sku_h_choice_name
           sku_v_choice_name
         end
+      end
+
+      delegate :code, to: :product, prefix: true, allow_nil: true
+
+      def product_code=(code)
+        return if product_code == code
+        self.product = Comable::Product.find_by(code: code)
       end
     end
   end
