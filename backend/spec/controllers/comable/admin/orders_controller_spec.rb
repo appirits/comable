26304a450565b7a678a0e1ffc035e0ce8a25805a
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-04-22 15:13:25 +0900

    Add tests for `cancel` and `resume` actions

--- backend/spec/controllers/comable/admin/orders_controller_spec.rb
+++ backend/spec/controllers/comable/admin/orders_controller_spec.rb
@@ -37,4 +37,78 @@ describe Comable::Admin::OrdersController do
       expect(response.content_type).to eq(Mime::XLSX)
     end
   end
+
+  describe 'POST cancel' do
+    let(:order) { FactoryGirl.create(:order, :completed) }
+
+    it 'cancel the requested order' do
+      post :cancel, id: order.to_param
+      order.reload
+      expect(order).to be_canceled
+    end
+
+    it 'restock the requested order' do
+      stock = FactoryGirl.create(:stock, :stocked, :with_product)
+      order_item = FactoryGirl.create(:order_item, stock: stock)
+      order.order_items << order_item
+
+      expect { post :cancel, id: order.to_param }.to change { stock.reload.quantity }.by(order_item.quantity)
+    end
+
+    it 'redirects to the order' do
+      post :cancel, id: order.to_param
+      expect(response).to redirect_to([comable, :admin, order])
+    end
+  end
+
+  describe 'POST resume' do
+    let(:order) { FactoryGirl.create(:order, :completed) }
+    let(:order_item) { FactoryGirl.create(:order_item, stock: stock) }
+    let(:stock) { FactoryGirl.create(:stock, :stocked, :with_product) }
+
+    before { order.cancel! }
+
+    context 'with resumable order' do
+      it 'resume the requested order' do
+        post :resume, id: order.to_param
+        order.reload
+        expect(order).to be_resumed
+      end
+
+      it 'unstock the requested order' do
+        order.order_items << order_item
+
+        expect { post :resume, id: order.to_param }.to change { stock.reload.quantity }.by(-order_item.quantity)
+      end
+
+      it 'redirects to the order' do
+        post :resume, id: order.to_param
+        expect(response).to redirect_to([comable, :admin, order])
+      end
+    end
+
+    context 'with out of stock' do
+      before do
+        order.order_items << order_item
+        stock.update_attributes(quantity: 0)
+      end
+
+      it 'keep the requested order cancel' do
+        post :resume, id: order.to_param
+        order.reload
+        expect(order).to be_canceled
+      end
+
+      it 'redirects to the order' do
+        post :resume, id: order.to_param
+        expect(response).to redirect_to([comable, :admin, order])
+      end
+
+      it 'assigns the message as flash[:alert]' do
+        post :resume, id: order.to_param
+        order.reload
+        expect(flash[:alert]).to include(Comable.t('errors.messages.out_of_stock', name: stock.name_with_sku))
+      end
+    end
+  end
 end
