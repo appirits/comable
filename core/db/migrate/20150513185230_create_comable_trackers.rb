8b033c3f2d6aed341a1e3e1bc27c1b99e597e69c
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-05-16 00:01:58 +0900

    Remove the unnecessary comment

--- core/db/migrate/20150513185230_create_comable_trackers.rb
+++ core/db/migrate/20150513185230_create_comable_trackers.rb
@@ -1,7 +1,6 @@
 class CreateComableTrackers < ActiveRecord::Migration
   def change
     create_table :comable_trackers do |t|
-      # TODO: Rename the column: activated_flag => activated
       t.boolean :activated_flag, null: false, default: true
       t.string :name, null: false
       t.string :tracker_id
