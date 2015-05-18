ca5de30071db706d92fe9c00a76a572e6d8d593c
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-05-19 04:43:17 +0900

    Implmenet the directory tree

--- backend/app/controllers/comable/admin/themes_controller.rb
+++ backend/app/controllers/comable/admin/themes_controller.rb
@@ -3,18 +3,51 @@ require_dependency 'comable/admin/application_controller'
 module Comable
   module Admin
     class ThemesController < Comable::Admin::ApplicationController
+      before_filter :load_directory_tree, only: :show
+
       def index
       end
 
       def show
         params[:id] ||= 'default'
-        @code = File.open("themes/#{params[:id]}/comable/products/index.slim").read
+        @code = File.read(filepath) if filepath && File.exist?(filepath)
       end
 
       def update
         params[:id] ||= 'default'
-        @code = params[:code]
-        render :show
+
+        File.write(filepath, params[:code])
+
+        redirect_to comable.admin_theme_path(id: params[:id], path: params[:path])
+      end
+
+      private
+
+      def filepath
+        return unless params[:path]
+        File.join("themes/#{params[:id]}/comable", params[:path])
+      end
+
+      def load_directory_tree
+        @directory_tree = directory_tree("themes/#{params[:id]}/comable")
+      end
+
+      def directory_tree(path, parent = nil)
+        children = []
+        tree = { (parent || :root)  => children }
+
+        Dir.foreach(path) do |entry|
+          next if entry.in? ['..', '.']
+          fullpath = File.join(path, entry)
+
+          if File.directory?(fullpath)
+            children << directory_tree(fullpath, entry.to_sym)
+          else
+            children << entry
+          end
+        end
+
+        tree
       end
     end
   end
---
+++ backend/app/helpers/comable/admin/themes_helper.rb
@@ -0,0 +1,19 @@
+module Comable
+  module Admin
+    module ThemesHelper
+      def display_directory_tree(tree, parent = nil)
+        key, value = tree.first
+        value.map do |entry|
+          if entry.is_a? Hash
+            parent_key, _ = entry.first
+            content_tag(:dt, parent_key) + display_directory_tree(entry, File.join(parent.to_s, parent_key.to_s))
+          else
+            fullpath = File.join(parent, entry)
+            link = link_to(entry, comable.admin_theme_path(path: fullpath))
+            content_tag(:dd, link)
+          end
+        end.join.html_safe
+      end
+    end
+  end
+end
