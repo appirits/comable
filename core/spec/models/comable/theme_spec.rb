a8aa4c7a9e57f06d2da0d97e62247cf4bcad3738
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-06-20 03:52:13 +0900

    Add tests for the model

--- core/spec/models/comable/theme_spec.rb
+++ core/spec/models/comable/theme_spec.rb
@@ -12,4 +12,28 @@ describe Comable::Theme, type: :model do
   it { is_expected.to validate_length_of(:description).is_at_most(255) }
   it { is_expected.to validate_length_of(:homepage).is_at_most(255) }
   it { is_expected.to validate_length_of(:author).is_at_most(255) }
+
+  describe '#default_version' do
+    it 'returns the string' do
+      expect(subject.default_version).to be_a(String)
+    end
+  end
+
+  describe '#to_param' do
+    it 'returns #name' do
+      expect(subject.to_param).to eq(subject.name)
+    end
+  end
+
+  describe '#display_name' do
+    it 'returns #display when #display is present' do
+      subject.display = 'Sample Theme'
+      expect(subject.display_name).to eq(subject.display)
+    end
+
+    it 'returns #name when #display is blank' do
+      subject.display = ''
+      expect(subject.display_name).to eq(subject.name)
+    end
+  end
 end
