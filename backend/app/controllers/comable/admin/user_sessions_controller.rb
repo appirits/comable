64af63bcb6bbd06bd9970a6e27db68ffd3c7d423
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-06-27 17:39:57 +0900

    Fix the problem that cannot call `page_name` method

--- backend/app/controllers/comable/admin/user_sessions_controller.rb
+++ backend/app/controllers/comable/admin/user_sessions_controller.rb
@@ -1,6 +1,7 @@
 module Comable
   module Admin
     class UserSessionsController < Devise::SessionsController
+      helper Comable::Admin::ApplicationHelper
       layout 'comable/admin/application'
     end
   end
