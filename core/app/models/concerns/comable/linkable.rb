79243093409df1fb261dec21fe5dda03370cfb02
ymatsumoto <ymatsumoto@appirits.com>
2015-07-29 16:43:36 +0900

    Navigation機能作成：管理画面：リンクに指定できるオプション追加

--- core/app/models/concerns/comable/linkable.rb
+++ core/app/models/concerns/comable/linkable.rb
@@ -3,12 +3,13 @@ module Comable
     extend ActiveSupport::Concern
 
     module ClassMethods
-      attr_reader :_linkable_columns, :use_index
+      attr_reader :_linkable_columns, :use_index, :index_only
 
       def linkable_columns_keys(options = {})
         if options.present?
           @_linkable_columns = default_columns_key.merge(options.slice(:name, :id))
           @use_index = options[:use_index]
+          @index_only = options[:index_only]
         else
           @_linkable_columns
         end
@@ -19,12 +20,14 @@ module Comable
       end
 
       def linkable_id_options
+        return [index_option] if @index_only
+
         # HACK: Rails3系のpluckでは複数フィールドを指定できないためselectとmapでカラムを取得する
         # options = pluck(*linkable_columns(:name, :id))
         columns = linkable_columns(:name, :id)
         records = select(columns)
         options = records.map(&columns.first).zip(records.map(&columns.last))
-        @use_index ? options.unshift([Comable.t('admin.actions.index'), nil]) : options
+        @use_index ? options.unshift(index_option) : options
       end
 
       def linkable_exists?
@@ -38,6 +41,10 @@ module Comable
           id: :id,
           name: :name
         }
+      end
+
+      def index_option
+        [Comable.t('admin.actions.index'), nil]
       end
     end
   end
