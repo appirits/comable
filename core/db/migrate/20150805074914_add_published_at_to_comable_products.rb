d263dd6f1a8908eac71b289f4e95b885e28e7faf
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-08-17 15:40:16 +0900

    Fix the problem that the existing product will be unpublished

--- core/db/migrate/20150805074914_add_published_at_to_comable_products.rb
+++ core/db/migrate/20150805074914_add_published_at_to_comable_products.rb
@@ -3,5 +3,9 @@ class AddPublishedAtToComableProducts < ActiveRecord::Migration
     change_table :comable_products do |t|
       t.datetime :published_at
     end
+
+    reversible do |dir|
+      dir.up { Comable::Product.update_all(published_at: Date.today.in_time_zone) }
+    end
   end
 end
