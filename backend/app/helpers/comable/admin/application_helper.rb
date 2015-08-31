e21c49eeebc6c07b0ef4db61e65e6606ced1ba3f
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-08-31 17:03:33 +0900

    Commonize `link_to_add_fields` helper method

--- backend/app/helpers/comable/admin/application_helper.rb
+++ backend/app/helpers/comable/admin/application_helper.rb
@@ -10,12 +10,9 @@ module Comable
         image_tag "//www.gravatar.com/avatar/#{hash}?default=mm", options
       end
 
-      def link_to_add_fields(name, f, association, options = {})
-        new_object = f.object.class.reflect_on_association(association).klass.new
-        fields = f.fields_for(association, new_object, child_index: "new_#{association}") do |builder|
-          render("comable/admin/shared/#{association.to_s.singularize}_fields", ff: builder)
-        end
-        link_to(name, 'javascript:void(0)', options.merge(onclick: "add_fields(this, '#{association}', '#{escape_javascript(fields)}')"))
+      def link_to_add_fields(name, f, type, options = {})
+        new_fields = build_fields(f, type)
+        link_to(name, 'javascript:void(0)', options.merge(class: "add_fields #{options[:class]}", 'data-field-type' => type, 'data-content' => "#{new_fields}"))
       end
 
       def button_to_remove_fields(name, options = {})
