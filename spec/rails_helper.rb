7d9a348d08ea1450f2107ae5d8d67ece3e63d612
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-09-07 15:46:44 +0900

    Change the settigns for DatabaseCleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,19 +56,19 @@ RSpec.configure do |config|
   #
   config.use_transactional_fixtures = false
 
-  # # Clean up database before test
-  # config.before(:suite) do
-  #   DatabaseCleaner.clean_with(:truncation)
-  # end
+  # Clean up database before test
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
   # Start the transaction
-  config.before(:example) do |example|
+  config.before(:each) do |example|
     DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
     DatabaseCleaner.start
   end
 
   # Clean up database by the strategy
-  config.after(:example) do
+  config.after(:each) do
     DatabaseCleaner.clean
   end
 end
