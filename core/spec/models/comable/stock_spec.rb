b4c9a3a406f8bf6e8efe60eb36fa7a6033c34bc5
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-09-07 16:59:54 +0900

    Add the test for deprecated methods

--- core/spec/models/comable/stock_spec.rb
+++ core/spec/models/comable/stock_spec.rb
@@ -1,3 +1,31 @@
 describe Comable::Stock do
   it { is_expected.to validate_numericality_of(:quantity).is_greater_than_or_equal_to(0) }
+
+  describe '#product=' do
+    it 'should sets product with vatiant' do
+      subject.variant = build(:variant)
+      subject.product = build(:product)
+      expect(subject.product).to eq(subject.variant.product)
+    end
+
+    it 'should sets product without vatiant' do
+      subject.variant = nil
+      subject.product = build(:product)
+      expect(subject.variant).to be_new_record
+    end
+  end
+
+  describe '#sku_v?' do
+    it 'should returns false when it has only option of variant' do
+      subject.variant = build(:variant, options: [name: 'Size', value: 'M'])
+      subject.product = build(:product)
+      expect(subject.sku_v?).to be false
+    end
+
+    it 'should returns true when it has two options of variant' do
+      subject.variant = build(:variant, options: [name: 'Size', value: 'M'] + [name: 'Color', value: 'Red'])
+      subject.product = build(:product)
+      expect(subject.sku_v?).to be false
+    end
+  end
 end
