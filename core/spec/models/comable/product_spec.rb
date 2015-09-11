1d7f4fe7dd669e827dd77675dc46528cce8cf54e
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-09-11 18:18:20 +0900

    Refactor the spec for Product

--- core/spec/models/comable/product_spec.rb
+++ core/spec/models/comable/product_spec.rb
@@ -38,20 +38,24 @@ describe Comable::Product do
 
   describe 'published?' do
     subject { build(:product) }
+
     it 'should be false when published_at is nil' do
       subject.published_at = nil
       expect(subject.published?).to be false
     end
+
     it 'should be false when published_at greater than now' do
-      subject.published_at = Time.now + 1
+      subject.published_at = 1.minute.since
       expect(subject.published?).to be false
     end
+
     it 'should be true when published_at equal now' do
       subject.published_at = Time.now
       expect(subject.published?).to be true
     end
+
     it 'should be true when published_at less than now' do
-      subject.published_at = Time.now - 1
+      subject.published_at = 1.minute.ago
       expect(subject.published?).to be true
     end
   end
