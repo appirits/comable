06e5c662c11753f31add7a1f2e2e3aea3b317451
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-09-25 11:05:01 +0900

    Add Comable namespace to ImageUploader

---
+++ core/app/uploaders/comable/image_uploader.rb
@@ -0,0 +1,9 @@
+module Comable
+  class ImageUploader < CarrierWave::Uploader::Base
+    storage :file
+
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{model.id}"
+    end
+  end
+end
--- core/app/uploaders/image_uploader.rb
+++
@@ -1,7 +0,0 @@
-class ImageUploader < CarrierWave::Uploader::Base
-  storage :file
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{model.id}"
-  end
-end
