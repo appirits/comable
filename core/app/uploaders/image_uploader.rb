801a68d2cdfba74e7757a844276f8679c8e8a330
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-09-25 11:03:05 +0900

    Change the image directory

--- core/app/uploaders/image_uploader.rb
+++ core/app/uploaders/image_uploader.rb
@@ -2,6 +2,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "#{model.class.to_s.underscore}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{model.id}"
   end
 end
