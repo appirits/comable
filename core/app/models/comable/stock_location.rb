a9b7732afb7f3f168a0daa94a528ffd629ce02c4
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-10-30 12:24:01 +0900

    Fix the failed test cases for Checkout

--- core/app/models/comable/stock_location.rb
+++ core/app/models/comable/stock_location.rb
@@ -13,6 +13,7 @@ module Comable
     class << self
       def default
         where(default: true).first_or_initialize do |stock_location|
+          # TODO: I18n
           stock_location.name = 'Default'
         end
       end
